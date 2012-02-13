2fe7722d0b55d8852e1abf2fed49a7ff7f5309a7
Mike Bryant <mikesname@gmail.com>
2012-02-13 14:07:00 +0000

    Add pythonpath for virtualenv/app in wsgi script

--- ehriimporter/wsgi.py
+++ ehriimporter/wsgi.py
@@ -1,7 +1,18 @@
-import os, sys
-sys.path.append('/usr/local/django')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ehriimporter.settings'
+import os
+import sys
 
-import django.core.handlers.wsgi
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, PROJECT_ROOT)
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../lib/python2.6/site-packages")))
 
-application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.handlers.wsgi import WSGIHandler
+
+import pinax.env
+
+# setup the environment for Django and Pinax
+pinax.env.setup_environ(__file__)
+
+
+# set application for WSGI processing
+application = WSGIHandler()
