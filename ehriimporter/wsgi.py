f6bf5feb0a2aac9fc5e7439288dcc2cc1ff3018c
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:42:51 +0000

    More tweaks to the wsgi file

--- ehriimporter/wsgi.py
+++ ehriimporter/wsgi.py
@@ -1,8 +1,10 @@
 import os, sys
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_NAME = os.path.basename(PROJECT_ROOT)
+
 sys.path.insert(0, PROJECT_ROOT)
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir)))
 
 venv_path = os.path.abspath(os.path.join(PROJECT_ROOT, "../../../"))
 activate_this = os.path.join(venv_path, "bin/activate_this.py")
@@ -10,7 +12,7 @@ execfile(activate_this, dict(__file__=activate_this))
 
 sys.stderr.write("WSGI Python Path (Importer): %s\n" % sys.path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ehriimporter.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % PROJECT_NAME
 
 import django.core.handlers.wsgi
 
