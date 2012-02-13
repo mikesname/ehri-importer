e3ead3a7294f2218ab053f7b929b8aaad138c451
Mike Bryant <mikesname@gmail.com>
2012-02-13 21:12:20 +0000

    Simplify validation view

--- ehriimporter/xlsimport/views.py
+++ ehriimporter/xlsimport/views.py
@@ -22,28 +22,16 @@ def save_to_temp(f):
 def validate(request):
     """Validate an XLS."""
     template = "xlsimport/validate.html"
-    if request.method == "POST":
-        form = forms.XLSForm(request.POST, request.FILES)
-        if not form.is_valid():
-            context = dict(form=form)
-            return render(request, template, context)
-        name = request.FILES["xlsfile"].name
-        temppath = save_to_temp(request.FILES["xlsfile"])
-        validator = getattr(xls, form.cleaned_data["xlstype"])(temppath)
-        try:
-            validator.validate()
-        except xls.XLSError:
-            pass
-        os.unlink(temppath)
-
-        # form is good, so render the report template
-        context = dict(form=form, errors=validator.errors,
-                name=name, validator=validator)
-        return render(request, template, context)
-
-
-
     form = forms.XLSForm()
     context = dict(form=form)
+    if request.method == "POST":
+        form = forms.XLSForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = request.FILES["xlsfile"].name
+            temppath = save_to_temp(request.FILES["xlsfile"])
+            validator = getattr(xls, form.cleaned_data["xlstype"])()
+            validator.validate(temppath)
+            os.unlink(temppath)
+            context.update(errors=validator.errors, validator=validator)
     return render(request, template, context)
 
