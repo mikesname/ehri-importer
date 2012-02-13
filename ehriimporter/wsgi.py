8f4abea073e7ac3b0b70119bb4daeb14f1760653
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:00:36 +0000

    Revert "More hacks to the wsqi path", got path wrong
    This reverts commit f5a24284ee17b8bc8f1792c35c6266be6668ca30.

--- ehriimporter/wsgi.py
+++ ehriimporter/wsgi.py
@@ -5,11 +5,6 @@ sys.path.insert(0, os.path.abspath(PROJECT_ROOT))
 sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../lib/python2.6/site-packages")))
 
-# Massive temporary hack... since this app depends on
-# the SQLAlchemy-qubit module, which is also far from
-# complete, add it's virtualenv path here
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "src/sqlaqubit")))
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ehriimporter.settings'
 
 import django.core.handlers.wsgi
