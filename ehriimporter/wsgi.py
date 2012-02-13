0dad359ca31349fefc0f9dfdeea98e4d31d1b04f
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:32:08 +0000

    Attempt to make wsgi script more virtualenv-friendly

--- ehriimporter/wsgi.py
+++ ehriimporter/wsgi.py
@@ -1,14 +1,11 @@
 import os, sys
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.abspath(PROJECT_ROOT))
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../lib/python2.6/site-packages")))
+sys.path.insert(0, PROJECT_ROOT)
 
-# Add temporary path for sqlaqubit module, which is pull as src from
-# github 'cos it's not finished yet
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../src/sqlaqubit")))
-
+venv_path = os.path.abspath(os.path.join(PROJECT_ROOT, "../../../"))
+activate_this = os.path.join(venv_path, "bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ehriimporter.settings'
 
