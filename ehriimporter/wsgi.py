53f2b8c2dadb8d30091c719702e537904e191c09
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:01:41 +0000

    More hacks to the wsqi path

--- ehriimporter/wsgi.py
+++ ehriimporter/wsgi.py
@@ -5,6 +5,11 @@ sys.path.insert(0, os.path.abspath(PROJECT_ROOT))
 sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../lib/python2.6/site-packages")))
 
+# Add temporary path for sqlaqubit module, which is pull as src from
+# github 'cos it's not finished yet
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../src/sqlaqubit")))
+
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ehriimporter.settings'
 
 import django.core.handlers.wsgi
