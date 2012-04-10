c132e5ed3bdc42e70d1aa80dc843242f16ec7490
Mike Bryant <mikesname@gmail.com>
2012-04-10 15:03:38 +0100

    Allow validating of choice-limited fields.

--- ehriimporter/xlsimport/validators.py
+++ ehriimporter/xlsimport/validators.py
@@ -28,13 +28,14 @@ ERROR_CODES = {
 
 
 def split_multiple(multistr, sep=",,"):
-    return [s for s in multistr.rsplit(sep) if s.strip()]
+    return [s for s in unicode(multistr).rsplit(sep) if s.strip()]
 
 
 class XLSField(object):
     def __init__(self, name, unique=False, multiple=False,
             default=None, required=False, date=False,
-            validate=False, i18n=False, type=None, help=None):
+            validate=False, i18n=False, type=None, help=None,
+            choices=None):
         self.name = name
         self.unique = unique
         self.multiple = multiple
@@ -45,6 +46,7 @@ class XLSField(object):
         self.type = type
         self.limit = None
         self.help = help
+        self.choices = choices
 
     def __unicode__(self):
         return self.name
@@ -93,6 +95,9 @@ class XLSSheetDefinition(object):
 
     def limited(self):
         return [f for f in self.fields.values() if f.limit]
+
+    def choices(self):
+        return [f for f in self.fields.values() if f.choices is not None]
 
 
 class XLSValidator(object):
@@ -154,6 +159,10 @@ class XLSValidator(object):
     def LIMITED(self):
         return [f.name for f in self.fielddef.limited()]
 
+    @property
+    def CHOICES(self):
+        return [f.name for f in self.fielddef.choices()]
+
     def open_xls(self, xlsfile):
         self.workbook = xlrd.open_workbook(xlsfile, formatting_info=True)
         try:
@@ -175,7 +184,7 @@ class XLSValidator(object):
 
     def coerce_bool(self, val):
         """Parse a boolean value, assuming "yes", "true", "1" etc -> True."""
-        if str(val).lower().strip() in ("yes", "true",):
+        if unicode(val).lower().strip() in ("yes", "true",):
             return True
         try:
             return int(val) == 1
@@ -242,6 +251,7 @@ class XLSValidator(object):
         self.check_person_names(rownum, rowdata)
         self.check_dates(rownum, rowdata)
         self.check_charfield_length(rownum, rowdata)
+        self.check_choices(rownum, rowdata)
 
     def check_required_columns(self):
         """Make sure there are no blanks where there shouldn't
@@ -252,7 +262,7 @@ class XLSValidator(object):
                     enumerate(self.sheet.col_slice(
                         col, self.HEADING_ROW, self.sheet.nrows))]
             for i, data in rowsdata:
-                if data is None or str(data).strip() == "":
+                if data is None or unicode(data).strip() == "":
                     self.add_error(i, "Missing value on required column: %s" % (
                         colhead))
 
@@ -311,11 +321,25 @@ class XLSValidator(object):
     def check_person_names(self, rownum, rowdata):
         for field in self.PERSONNAMES:
             for item in split_multiple(rowdata.get(field, "")):
+                if item.startswith("[org] "):
+                    continue
                 if item.count(",") < 1:
                     self.add_error(rownum, "No 'comma' delimiting surname/given name in person name field '%s': '%s'" %
                             (field, item))
                 elif item.count(",") > 1:
                     self.add_error(rownum, "Multiple 'commas' in name field '%s': '%s'" % (field, item))
+
+    def check_choices(self, rownum, rowdata):
+        """Check fields that must contain choices are limited to
+        the appropriate values."""
+        for fieldobj in self.fielddef.choices():
+            name = fieldobj.name
+            choices = fieldobj.choices
+            if rowdata[name] not in choices:
+                self.add_error(rownum,
+                        "Invalid value for field '%s': '%s'. Must be one of: %s" % (
+                            name, rowdata[name], ", ".join(["'%s'" % s for s in choices])))
+
 
     def check_dates(self, rownum, rowdata):
         """Check dates are in YYYY-MM-DD format.  A preceding 'c' for
@@ -324,6 +348,9 @@ class XLSValidator(object):
             for datestr in split_multiple(rowdata[field]):
                 if datestr.startswith("c"):
                     datestr = datestr[1:]
+                # hack! to rid int dates of coerced float points
+                if datestr.endswith(".0"):
+                    datestr = datestr[:-2]
                 try:
                     parser.parse(datestr, yearfirst=True)
                 except ValueError:
